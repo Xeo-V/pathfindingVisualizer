03ba9f93f6063848b711a8482111a50dfac7ec8f
X30 <74484800+Xeo-V@users.noreply.github.com>
2023-09-22 13:21:10 +0530

    Update core.py
    -core comeplte

--- core.py
+++ core.py
@@ -1,49 +1,48 @@
-# core.py
-
 import tkinter as tk
 from tkinter import ttk
 from working import dijkstra, a_star, bfs, dfs
+import threading
 
 class PathfindingVisualizer:
     def __init__(self, master):
         self.master = master
         self.master.title("Pathfinding Visualizer")
-        self.algorithm = tk.StringVar(value="Dijkstra")  # Default algorithm
-        self.rows = 20  # Number of rows in the grid
-        self.cols = 20  # Number of columns in the grid
-        self.cell_size = 25  # Size of each cell in pixels
-        self.start = None  # Start point coordinates
-        self.end = None  # End point coordinates
-        self.mode = None  # Mode can be 'start', 'end', 'wall', or None
+        self.algorithm = tk.StringVar(value="Dijkstra") 
+        self.rows = 30  
+        self.cols = 80  
+        self.cell_size = 1
+        self.start = None 
+        self.end = None 
+        self.mode = None 
         self.create_widgets()
         
     def create_widgets(self):
-        # Create dropdown to select the algorithm
-        algo_label = tk.Label(self.master, text="Algorithm:")
+        control_frame = tk.Frame(self.master)
+        control_frame.pack(side="top", fill="x")
+        
+        algo_label = tk.Label(control_frame, text="Algorithm:")
         algo_label.pack(side="left")
         
-        algo_dropdown = ttk.Combobox(self.master, textvariable=self.algorithm, values=["Dijkstra", "A*", "BFS", "DFS"])
+        algo_dropdown = ttk.Combobox(control_frame, textvariable=self.algorithm, values=["Dijkstra", "A*", "BFS", "DFS"])
         algo_dropdown.pack(side="left")
         
-        # Create buttons to set start, set end, visualize, and reset
-        start_button = tk.Button(self.master, text="Set Start", command=self.set_start)
+        start_button = tk.Button(control_frame, text="Set Start", command=self.set_start)
         start_button.pack(side="left")
         
-        end_button = tk.Button(self.master, text="Set End", command=self.set_end)
+        end_button = tk.Button(control_frame, text="Set End", command=self.set_end)
         end_button.pack(side="left")
         
-        visualize_button = tk.Button(self.master, text="Visualize", command=self.visualize)
+        visualize_button = tk.Button(control_frame, text="Visualize", command=self.visualize)
         visualize_button.pack(side="left")
         
-        reset_button = tk.Button(self.master, text="Reset", command=self.reset)
+        reset_button = tk.Button(control_frame, text="Reset", command=self.reset)
         reset_button.pack(side="left")
         
-        # Create the grid
         self.create_grid()
         
     def create_grid(self):
         self.grid_frame = tk.Frame(self.master)
-        self.grid_frame.pack(side="bottom")
+        self.grid_frame.pack(side="top", fill="both", expand=True)
         
         self.grid = []
         for row in range(self.rows):
@@ -82,49 +81,51 @@ class PathfindingVisualizer:
         self.master.config(cursor='cross')
         
     def visualize(self):
-        # Check if start and end points are set
-        if self.start is None or self.end is None:
-            tk.messagebox.showwarning("Warning", "Please set both start and end points.")
-            return
-    
-        # Create a grid representation with True for walls and False for open spaces
-        grid = [[self.grid[row][col].cget("bg") == "black" for col in range(self.cols)] for row in range(self.rows)]
-    
-        # Get the selected algorithm from the dropdown
-        algorithm = self.algorithm.get()
-    
-        # Call the corresponding pathfinding algorithm from working.py
-        if algorithm == "Dijkstra":
-            path = dijkstra(grid, self.start, self.end)
-        elif algorithm == "A*":
-            path = a_star(grid, self.start, self.end)
-        elif algorithm == "BFS":
-            path = bfs(grid, self.start, self.end)
-        elif algorithm == "DFS":
-            path = dfs(grid, self.start, self.end)
-        else:
-            tk.messagebox.showerror("Error", f"Unknown algorithm {algorithm}")
-            return
-    
-        # Visualize the resulting path
-        if not path:
-            tk.messagebox.showinfo("Info", "No path found.")
-            return
-    
-        for row, col in path:
-            self.grid[row][col].config(bg="blue")
-            self.master.update_idletasks()
-            self.master.after(50)  # Add a delay to visualize the path being constructed
+        threading.Thread(target=self.run_visualization).start()
 
+    def run_visualization(self):
+        try:
+            if self.start is None or self.end is None:
+                tk.messagebox.showwarning("Warning", "Please set both start and end points.")
+                return
+            
+            grid = [[self.grid[row][col].cget("bg") == "black" for col in range(self.cols)] for row in range(self.rows)]
+            
+
+            algorithm = self.algorithm.get()
+            
+
+            if algorithm == "Dijkstra":
+                path = dijkstra(grid, self.start, self.end)
+            elif algorithm == "A*":
+                path = a_star(grid, self.start, self.end)
+            elif algorithm == "BFS":
+                path = bfs(grid, self.start, self.end)
+            elif algorithm == "DFS":
+                path = dfs(grid, self.start, self.end)
+            else:
+                tk.messagebox.showerror("Error", f"Unknown algorithm {algorithm}")
+                return
+            
+            if not path:
+                tk.messagebox.showinfo("Info", "No path found.")
+                return
+            
+            for row, col in path:
+                self.master.after(0, lambda r=row, c=col: self.grid[r][c].config(bg="blue"))
+                self.master.update_idletasks()
+                self.master.after(50)
         
+        except Exception as e:
+            tk.messagebox.showerror("Error", f"An error occurred: {e}")
+            
+               
     def reset(self):
-        # Reset the start and end points
         self.start = None
         self.end = None
         self.mode = None
         self.master.config(cursor='arrow')
 
-        # Clear the grid
         for row in range(self.rows):
             for col in range(self.cols):
                 self.grid[row][col].config(bg="white")
